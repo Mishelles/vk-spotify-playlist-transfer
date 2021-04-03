0157ebef1930f99fcca0c7419fc5098f6db2a690
SPerekrestova <sherlock.nacht@gmail.com>
2021-04-03 21:01:42 +0300

    Code cleanup

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ import logging
 # logging.basicConfig(level=logging.DEBUG)
 
 '''
+    Execute steps:
+        1. Vk part (getting token, getting tracks from VK);
+        2. Getting root token for Spotify
+        3. Creating playlists in Spotify
+        4. Adding tracks to Spotify
+
     TODO:
         1. Add regexp to clean track's name ('/' - is not valid)
             1.1 everything enclosed in brackets - \([^)]*\)\W
