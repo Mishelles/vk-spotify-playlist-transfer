90726fa78806e1a65885a0f7c570bc2dab35c749
SPerekrestova <sherlock.nacht@gmail.com>
2021-04-16 21:36:42 +0300

    Finished button

--- frontend/src/components/ButtonComponent.js
+++ frontend/src/components/ButtonComponent.js
@@ -1,13 +1,31 @@
-import {Button, Grid} from "@material-ui/core";
+import {Button, emphasize, Grid, makeStyles} from "@material-ui/core";
 import {Link} from "react-router-dom";
 import React from "react";
 
+const useStyles = makeStyles({
+    root: {
+        background: '#1DB954',
+        boxShadow: '0 0 35px 3px rgba(0, 0, 0, 0.29)',
+        borderRadius: '35px',
+        color: '#FFF',
+        marginTop: '100px',
+        "&:hover, &:focus": {
+            background: '#14843e'
+        },
+        width: '250px',
+    },
+});
+
 export default function ButtonComponent(props) {
+    const classes = useStyles();
+
     return (
-        <Grid container justify="center" alignItems="center" alignContent="center" direction="column">
-            <Grid item xs={6} >
+        <Grid container justify="center" alignItems="center" alignContent="center" direction="column" >
+            <Grid item xs={6}>
                 <Link to={props.link}>
-                    <Button variant="contained" color="primary">
+                    <Button variant="contained" color="inherit" classes={{
+                        root: classes.root,
+                    }}>
                         {props.text}
                     </Button>
                 </Link>
--- frontend/src/pages/WelcomeScreen.js
+++ frontend/src/pages/WelcomeScreen.js
@@ -1,21 +1,13 @@
-import {Button, Grid} from "@material-ui/core";
-import {Link} from "react-router-dom";
 import DescriptionComponent from "../components/DescriptionComponent";
+import ButtonComponent from "../components/ButtonComponent";
+import React from "react";
 
 
 export default function WelcomeScreen() {
     return (
         <div className="main">
             <DescriptionComponent text="Hello! This app will help you to transfer your VK music into Spotify playlist in 3 easy steps" />
-            <Grid container justify="center" alignItems="center" alignContent="center" direction="column">
-                <Grid item xs={6} >
-                    <Link to="/spotify-login">
-                        <Button variant="contained" color="primary">
-                            LET'S GO
-                        </Button>
-                    </Link>
-                </Grid>
-            </Grid>
+            <ButtonComponent text="LET'S GO" link="/spotify-login"/>
         </div>
     )
 }
