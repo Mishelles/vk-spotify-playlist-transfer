b94e1673f095006b1ed1f2358dece12c79e95729
Mishelles <sergeenkov.michael@gmail.com>
2021-04-19 13:22:32 +0300

    API method for spotify authorization with auth token

--- backend/backend.py
+++ backend/backend.py
@@ -1,41 +1,41 @@
+import os
+
 import yaml
 
 import requests
+from fastapi import FastAPI, HTTPException
 
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
 with open('creds.yaml', 'r') as c:
     config = yaml.safe_load(c)
 
-SPOTIFY_REDIRECT_URL = 'http://localhost:8000/spotify-login-callback'
-SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize?client_id={}&response_type=code&redirect_uri={}&scope=playlist-modify-private'
+SPOTIFY_REDIRECT_URL = os.environ.get('SPOTIFY_REDIRECT_URL', 'http://localhost:3000/spotify-callback')
 
 
-@app.get("/spotify-login")
-def spotify_login():
-    return RedirectResponse(url=SPOTIFY_AUTH_URL.format(config['sp_client_id'], SPOTIFY_REDIRECT_URL))
+class SpotifyLoginDto(BaseModel):
+    code: str
 
 
-@app.get("/spotify-login-callback")
-def spotify_login_callback(code: str):
+@app.post("/login/spotify", status_code=200)
+def login_to_spotify(dto: SpotifyLoginDto):
     response = requests.post(
         url='https://accounts.spotify.com/api/token',
         data={
             'grant_type': 'authorization_code',
-            'code': code,
+            'code': dto.code,
             'redirect_uri': SPOTIFY_REDIRECT_URL
         },
         headers={
             "Authorization": 'Basic {}'.format(config.get('sp_basic_auth'))
         }).json()
-    config['sp_access_token'] = response['access_token']
-    config['sp_refresh_token'] = response['refresh_token']
-    with open('creds.yaml', 'w') as c:
-        yaml.safe_dump(config, c)
-    return {
-        'access_token': response['access_token'],
-        'refresh_token': response['refresh_token']
-    }
+    try:
+        config['sp_access_token'] = response['access_token']
+        config['sp_refresh_token'] = response['refresh_token']
+    except KeyError:
+        raise HTTPException(status_code=400, detail='Invalid code provided')
+    else:
+        with open('creds.yaml', 'w') as file_object:
+            yaml.safe_dump(config, file_object)
